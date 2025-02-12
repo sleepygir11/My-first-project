117fa82806b38aa84dffc467d0a86bd6691f1c32
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2025-02-12 19:07:41 +0300

    Выводит True в том случае, если все скобки закрыты

--- file 3.py
+++ file 3.py
@@ -1,12 +1,17 @@
 def check_string_brackets():
+   i = 1
    b = input("Введите скобочки")
-   if len(b)%2 == 1:
-      print("False")
-   elif b[0] == ")":
-      print("False")
-   elif b[0] == b[-1]:
-      print("No")
-   else:
-      print("OK")
+   while i != len(b):
+      if b[0] == ")":
+         print("False")
+         break
+      elif b[i] == b[-i-1]:
+         print("False")
+         break
+      else: 
+         print("True")
+         break
+   i += 1
+
 check_string_brackets()
       
