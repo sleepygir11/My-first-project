ceb5d4193cd858ac4c0919add09350f6b6e1c0b8
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2025-03-05 17:59:59 +0300

    JOE PANCAKES

--- PROJECT 1.py
+++ PROJECT 1.py
@@ -7,11 +7,18 @@ H = 800
 g = random.randrange(0,600)
 q = random.randrange(0,600)
 win = pg.display.set_mode((W,H))
+
+font_size = 48
+font = pg.font.Font(None, font_size)
+
+
 G = random.choices(range(0,256), k = 3)
 img = pg.image.load("JOE.jpg")
 count = 0
 x = 0
 y = 0
+
+
 while True:
    for i in pg.event.get():
       if i.type == pg.QUIT:
@@ -46,15 +53,24 @@ while True:
       img_center = (img_pos[0] + img_size[0] // 2, img_pos[1] + img_size[1] // 2)
       distance = ((circle_pos[0] - img_center[0]) ** 2 + (circle_pos[1] - img_center[1]) ** 2) ** 0.5
 
-   if distance < circle_radius + (img_size[0] // 2) and circle_visible:
+   if distance < circle_radius + (img_size[0] // 2):
       circle_visible = False
       count += 1
 
    elif circle_visible:
       pg.draw.circle(win,(0,0,0), circle_pos, circle_radius)
 
+   if count == 1:
+      number = 1
+   else:
+      number = 0
+      
+   text_surface = font.render(str(number), True, (255,255,255))
+   text_rect = text_surface.get_rect(height = 0, width = 0)
+
 
    win.blit(img,(x,y))
+   win.blit(text_surface, text_rect)
    pg.display.update()
    pg.time.delay(100)
    # if distance_squared < (my_circle.radius ** 2) and my_circle.visible:
