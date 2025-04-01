10ba1ef94910fe5c94d040299b6e377dc9d3b9cf
iliyasupermonstr <94247348+iliyasupermonstr@users.noreply.github.com>
2025-04-01 19:54:02 +0300

    Смарт попугай

--- TeleBot 11.py
+++ TeleBot 11.py
@@ -1,7 +1,17 @@
 import telebot
 from collections import defaultdict
+import os,random
 
 bot = telebot.TeleBot("")
+
+MEDIA_FOLDER = "bot_media"
+
+if not os.path.exists(MEDIA_FOLDER):
+   os.makedirs(MEDIA_FOLDER)
+   with open(os.path.join(MEDIA_FOLDER, "test.txt"),"w") as f:
+      f.write('Это тестовый файл от вашего бота!')
+
+
 
 user_stats = defaultdict(lambda : {'repeat_count' : 0, 'last_message ': ''})
 
@@ -19,5 +29,27 @@ def show_stats(message):
                     f'Повторений: {stats['repeat_count']}\n'
                     f"Последнее сообщение: '{stats['last_message']}'")
 
+@bot.message_handler(commands = ['pic'])
+def send_random_pic(message):
+   pics = []
+   bot.send_photo(message.chat.id,random.choice(pics),
+                  caption = "Вот тебе случайная картинка!")
+   
+   
+@bot.message_handler(func = lambda message : True)
+def smart_parrot(message):
+   user_id = message.from_user.id
+   user_stats[user_id]['repeat_count']+= 1
+   user_stats[user_id]['last_message'] = message.text
+   print('Пользователь отправил', message.text)
+
+   if '?' in message.text:
+      response = f'🦜Ты спросил ({user_stats[user_id]['repeat_count']}): {message.text}'
+   elif "!" in message.text:
+      response = f'🦜Ты воскликнул ({user_stats[user_id]['repeat_count']}) : {message.text}'
+   else:
+      response = f'🦜Повторяю ({user_stats[user_id]['repeat_count']}) : {message.text}'
+
+   bot.send_message(message.chat.id, response)
 
 bot.polling()
